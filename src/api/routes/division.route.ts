67787c5e2c6e38d956bfce5e7bb9a7dd060e1533
Dmitriy Izotov <disaythis@gmail.com>
2023-05-22 17:52:32 +0300

    fix: naming

--- src/api/routes/division.route.ts
+++ src/api/routes/division.route.ts
@@ -1,18 +1,18 @@
 import { CreateDivisionDTO, FilterDivisionClassificator } from 'api/dto/classificator.dto.js';
 import { Request, Response, Router } from 'express';
 import * as classificatorController from '../controllers/classificator/classificator.controller.js';
-const classificatorRouter = Router();
+const divisionRouter = Router();
 
-classificatorRouter.get('/division', async (req: Request, res: Response) => {
+divisionRouter.get('/', async (req: Request, res: Response) => {
 	const filters: FilterDivisionClassificator = req.query;
 	const results = await classificatorController.getDivisionAll(filters);
 	return res.status(200).send(results);
 });
 
-classificatorRouter.post('/division/create', async (req: Request, res: Response) => {
+divisionRouter.post('/create', async (req: Request, res: Response) => {
 	const payload: CreateDivisionDTO = req.body;
 	const result = await classificatorController.DivisionCreate(payload);
 	return res.status(200).send(result);
 });
 
-export default classificatorRouter;
+export default divisionRouter;
